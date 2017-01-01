9ab9c362808bdbff7b5334925e9ceb46ea9ee0a9
user <user>
2017-01-01 17:22:27 +0800

    down-level transformations for async functions.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ts2.1-test
 
+npm install typescript
+
+options:
+npm install tslib
+
 https://github.com/Microsoft/TypeScript/wiki/Roadmap#21-december-2016
+
+
---
+++ asyncAwait.ts
@@ -0,0 +1,21 @@
+// Down-level transformations for Async Functions
+// https://github.com/Microsoft/TypeScript/pull/9175
+
+function delay(){
+    return new Promise(resolve=>{
+        setTimeout(resolve, 5000);
+    });
+}
+
+async function doWork(){
+    console.log("do work start");
+
+    for(let i=0;i<=3;i++){
+        await delay();
+        console.log(i);
+    }
+
+    console.log("do work completed!");
+}
+
+doWork();
